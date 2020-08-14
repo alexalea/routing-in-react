bc544bd8dae5b4156c76b76b7d3734f0e419f9c6
Alex <alexander.alea@hotmail.com>
2020-08-14 14:41:17 -0500

    Second Commit

---
+++ src/components/pageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PageNotFound extends React.Component {
+    render() {
+        return (
+            <div className="PageNotFound">Page Not Found</div>
+        )
+    }
+}
+
+export default PageNotFound
